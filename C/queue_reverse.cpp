971455dd67ae155e9fd182453ca012c1cf557a9f
Fernando Schettini <80331486+FernandoSchett@users.noreply.github.com>
2021-09-30 12:11:44 -0300

    Create queue_reverse.cpp
    library using exercise

---
+++ C/queue_reverse.cpp
@@ -0,0 +1,28 @@
+// Made by: Fernando Schettini Github: https://github.com/FernandoSchett
+
+// This program is gonna receive values, put then in a queue and then print it 
+// with the values in their reverse order.
+#include <stdio.h>
+#include "pilha.h"
+#include "queue.h"
+
+
+void reverse_queue(){ //this fucntion is gonna reverse the order of the queue values.
+	
+	
+
+	
+}
+
+int main(){
+	int number;
+	tp_queue Q1; 
+	printf("How many values do you wanna put in the queue?\n");
+	scanf(" %d", &number);
+	start_queue(&Q1);
+	fill_queue(&Q1, &number);
+	reverse_queue(&Q1);
+	printf("Reversed Queue:\n");
+	print_queue(&Q1);
+	return 0;
+}
