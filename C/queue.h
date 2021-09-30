d804b7a555c04a6ab200b1569370075168f7a4cd
Fernando Schettini <80331486+FernandoSchett@users.noreply.github.com>
2021-09-30 17:46:25 -0300

    some adjustments

--- C/queue.h
+++ C/queue.h
@@ -2,8 +2,9 @@
 // Made by: Fernando Schettini Github: https://github.com/FernandoSchett
 // Queues here  are "FIFO" (First IN, First OUT)
 
+
 #ifndef QUEUE_H   
-#define QUEUE_H   //indicates QUEUE_H library start
+#define QUEUE_H  //indicates QUEUE_H library start
 
 #include <stdio.h> //added stdio.h library.
 #define MAX 100 // every time "MAX" appears, compilator will read "100".
@@ -16,28 +17,29 @@ typedef struct{  //Defining a new variable, tp_queue.
     int size;					// finally, it need a integer that keeps  the queue size.
 } tp_queue;		
 
-void queue_starter(tp_queue *f){ // Function that receives a queue then reset start, size and end. 
+
+void start_queue(tp_queue *f){   // Function that receives a queue then reset start, size and end. 
 	
     f -> start = f -> end = MAX-1;
     f -> size = 0;
     
 }
 
-int empty_queue(tp_queue *f){ // Function that receive a queue, then verifies if its empty.
+int empty_queue(tp_queue *f){  // Function that receive a queue, then verifies if its empty.
 	
-    if(f -> start == f-> fim){
-		return 1; //"1" == Success
+    if(f -> start == f-> end){
+		return 1;  //"1" == Success
 	}
-    return 0; // "0" == Failure
+    return 0;  // "0" == Failure
     
 }
 
 int next(int pos){ //Function that receives a integer then returns the next interger. Used for returning next queue elements positions. 
 	
-    if(pos >= MAX -1){ // It will fall for pos >= 99, cuz an queue element position cannot be at 100th position.
-		return 0; //0 = failure
+    if(pos >= MAX -1){  // It will fall for pos >= 99, cuz an queue element position cannot be at 100th position.
+		return 0;  //0 = failure
 	}
-    return ++pos; 			//note: "++pos" != "pos++"
+    return ++pos; 	//note: "++pos" != "pos++"			
 
 }
 
@@ -47,37 +49,37 @@ int full_queue(tp_queue *f){ //Function that receives a queue and verifies if it
         return 1;
     }
     return 0;
-
+    
 	// 0 = not full.
 	// 1 = full.
+
 }
 
 int queue_insert(tp_queue *f, tp_item e){ //Function that receives a queue and a integer
     
 	if(full_queue(f)){
-		return 0; //failure,you cant add elements from a full queue.
+		return 0; //failure,you cant add elements from a full queue. 
 	}
 	f -> end = next(f -> end);
     f -> item[f->end] = e;
     f -> size++;
-    return 1; //success.
+    return 1;  //success.
 
 }
 
 int queue_remove(tp_queue *f, tp_item *e){ //Function that receives a queue and a integer, it will remove the queue first element and keep that value with integer "e"
 	
     if(empty_queue(f)){
-		return 0; //failure,you cant remove elements from a empty queue.
+		return 0;  //failure,you cant remove elements from a empty queue.
 	}	
-    f -> start = proximo(f->start);
+    f -> start = next(f->start);
     *e = f -> item[f->start];
     f -> size--;
-    return 1; //success.
+    return 1;  //success.
     
 }
-
-// You need to creat an copy from the orignal queue, to keep it originality.
-void queue_printer(tp_queue f){ //Function that receives a queue and print all elements.
+// You need to creat an copy from the orignal queue, to keep the originality.
+void print_queue(tp_queue f){ //Function that receives a queue and print all elements.
 
     tp_item e;
     while(!empty_queue(&f)){
@@ -93,6 +95,17 @@ int queue_size(tp_queue *f){ //Function that receives a queue, and return its si
     
 }
 
+void fill_queue(tp_queue *f, int *number){ //this function will receive the queue, then fill it then fill it with "number" elements.
+	int cont, value;
+	printf("Type the values: (Max numbers values: MAX)\n");
+	cont = 0;
+	while(cont < *number){
+		printf("Type the element:");
+		scanf(" %d", &value);
+		queue_insert(f, value);
+		cont++;
+	}
+}
 
 #endif // indicates QUEUE_H library ends
 
