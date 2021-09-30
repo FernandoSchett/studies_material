d44770de1d42e4b9ec9f7b506f2ee3cbc8d47d73
Fernando Schettini <80331486+FernandoSchett@users.noreply.github.com>
2021-09-30 14:03:01 -0300

    Making it work

--- C/Chooser.cpp
+++ C/Chooser.cpp
@@ -4,47 +4,34 @@
 // if the read number if odd, it will remove the last number stored. If the queue is empty, show a message instead.
 #include <stdio.h>
 #include "stack.h"
-//#include "queue.h"
-
-typedef struct{  // New Queue, MAX elements numbers == 5.
-    tp_item item[6];			
-    int start, end;				
-    int size;					
-} tp_data;	
-
-void start_data(tp_dados *f){ 
-	
-    f -> start = f -> end = 5;
-    f -> size = 0;
-    
-}
+#include "queue.h"
 
 int main(){
 	tp_item value, cont, element;
-	tp_data Q1;
-	start_data(&Q1);
+	tp_queue Q1;
+	start_queue(&Q1);
 	cont = 0;
 	while(cont < 5){
-		scanf(" %d", value);
+		scanf(" %d", &value);
 		if(value%2 == 0){ //pair, so keep it on the queue.
 			if(full_queue(&Q1)){ 
-				queue_remove(&Q1, element);
-				queue_insert(&Q1,value);
+				queue_remove(&Q1, &element);
+				queue_insert(&Q1, value);
 			}else{
 				queue_insert(&Q1,value);
 			}
 		}else{ //odd
 			if(empty_queue(&Q1)){
-				print("The queue is empty.");
+				printf("The queue is empty.");
 			}
 			else{
-				queue_remove(&Q1, element);	
+				queue_remove(&Q1, &element);	
 			}
-		
 		}
 		cont++;
 	}
-	
+	printf("New Queue");
+	print_queue(Q1);
 	return 0;
 }
 
