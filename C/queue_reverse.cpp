e1cd42bb8f5fa572193002f0c7cea4345070c05a
Fernando Schettini <80331486+FernandoSchett@users.noreply.github.com>
2021-09-30 12:42:09 -0300

    Update queue_reverse.cpp

--- C/queue_reverse.cpp
+++ C/queue_reverse.cpp
@@ -16,13 +16,13 @@ void reverse_queue(tp_queue *Q1){ //this fucntion is gonna reverse the order of 
 
 int main(){
 	int number;
-	tp_queue f; 
+	tp_queue Q1; 
 	printf("How many values do you wanna put in the queue?\n");
 	scanf(" %d", &number);
-	start_queue(&f);
-	fill_queue(&f, &number);
-	reverse_queue(&f);
+	start_queue(&Q1);
+	fill_queue(&Q1, &number);
+	reverse_queue(&Q1);
 	printf("Reversed Queue:\n");
-	print_queue(&f);
+//	print_queue(&Q1);
 	return 0;
 }
