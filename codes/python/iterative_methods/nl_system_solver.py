d07e2a24e207231b14a923331ac5c5227e23ccee
FernandoSchett <fernandoschettini@outlook.com>
2023-09-05 11:28:43 -0300

    [:sparkles:FEAT]: new Nl system solver

--- codes/python/iterative_methods/nl_system_solver.py
+++ codes/python/iterative_methods/nl_system_solver.py
@@ -9,13 +9,12 @@ Usage:
 
 import numpy as np
 
-class System_solver:
+class NL_system_solver:
     
-    def __init__(self, coef, indep, jacob=False, flag=False):
-        self.coef = coef
-        self.indep = indep
+    def __init__(self, func, jacob, flag=False):   
+        self.func = func
         self.jacob = jacob
-        self.flag = False
+        self.flag = flag
 
     def show_info(self, x_new, x, iter):
         if self.flag == True:
@@ -29,3 +28,28 @@ class System_solver:
                 print(f'  Relative error: {rel_error}')
             print(f'Variables: {x_new}')
             print('###############################################')
+        
+    def newton_system(self, x0, tol=1e-6, max_iter=100):
+        x = np.array(x0, dtype=float)
+        for iter in range(max_iter):
+            F = self.func(x)
+            J = self.jacob(x)
+            delta_x = np.linalg.solve(J, -F)
+            self.show_info(x + delta_x, x, iter)
+            x += delta_x
+            if np.linalg.norm(delta_x, np.inf) < tol:
+                return x
+        return x
+    
+if __name__ == "__main__":
+    def func(x):
+        return np.array([x[0]**2 + x[1]**2 - 1, x[0] + x[1] - 2])
+
+    def jacob(x):
+        return np.array([[2 * x[0], 2 * x[1]], [1, 1]])
+
+    solver = NL_system_solver(func, jacob) 
+    initial_guess = [0.5, 1.5]
+    solution = solver.newton_system(initial_guess)
+    print("Solution:", solution)
+
